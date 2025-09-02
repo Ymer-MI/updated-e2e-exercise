07796f4dbe93ed80aa00f59833d66fe321fcd96d
ymer94 <ymer_94@hotmail.com>
2025-09-02 14:14:26 +0200

    Added first test.
    Changes to be committed:
    	modified:   cypress/e2e/Todo.cy.ts

--- cypress/e2e/Todo.cy.ts
+++ cypress/e2e/Todo.cy.ts
@@ -2,4 +2,14 @@ describe('Todo App', () => {
     beforeEach(() => {
         cy.visit('/');
     })
+
+    it('can add a new todo', () => {
+        const input = cy.get('#newTodoText').should('exist'), button = cy.get('#newTodoForm button').first().should('exist').should('have.text', 'Skapa')
+
+        input.type('New Todo')
+        button.click()
+
+        cy.get('#todos').children().should('have.length', 1).first().should('have.text', 'New Todo').and('have.class', 'todo__text').and('not.have.class', 'todo__text--done')
+
+    })
 })
